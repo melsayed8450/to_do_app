361ce215063b9c9d0c967a07ca3934450391f683
melsayed8450 <100236901+melsayed8450@users.noreply.github.com>
2024-01-25 16:25:11 +0200

    Removed Remote routes

--- lib/common/routes/remote_routes.dart
+++ lib/common/routes/remote_routes.dart
@@ -1,5 +1,1 @@
-class AppRemoteRoutes {
-  static const String someThingWentWrong = "Something went wrong.";
-  static const String internetConnectionMsg = "Something went wrong!! Please check internet connection.";
-  static const String baseUrl = "https://melsayed845.pythonanywhere.com/";
-}
+class AppRemoteRoutes {}
